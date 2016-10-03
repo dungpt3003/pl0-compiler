d08db21e62910e38f945c5d569203c4d7fd20eef
dungpt3003 <dungpt3003@gmail.com>
2016-10-03 23:43:19 +0700

    Complete header files | declare variables, keywords

--- src/pl0.h
+++ src/pl0.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+
+#ifndef PLO_H_
+#define PL0_H_
 #define MAX_IDENT_LEN 10
 
 typedef enum {
@@ -8,3 +11,23 @@ typedef enum {
 	NEQ, LSS, LEQ, GTR, GEQ, LPARENT, RPARENT, LBRACK, RBRACK, PERIOD, COMMA,
 	SEMICOLON, ASSIGN, PERCENT
 } TokenType;
+
+typedef char string[MAX_IDENT_LEN + 1];
+
+FILE* infile, outfile;
+char current_character;
+TokenType current_token;
+int number_ident;
+char Ident[MAX_IDENT_LEN + 1];
+
+// Keyword
+TokenType keyword[] = {BEGIN, CALL, CONST, DO, ELSE, END, FOR, IF, ODD, PROCEDURE, PROGRAM, THEN, TO, VAR, WHILE};
+string word[] = {"BEGIN", "CALL", "CONST", "DO", "ELSE", "END", "FOR", "IF", "ODD", "PROCEDURE", "PROGRAM", "THEN", "TO", "VAR", "WHILE"};
+
+// Token
+TokenType alltoken[] = {IDENT, NUMBER, PLUS, MINUS, TIMES, SLASH, EQU, NEQ, LSS, LEQ, GTR, GEQ, LPARENT,
+						RPARENT, LBRACK, RBRACK, PERIOD, COMMA, SEMICOLON, ASSIGN, PERCENT};
+string tokenword[] = {"IDENT", "NUMBER", "PLUS", "MINUS", "TIMES", "SLASH", "EQU", "NEQ", "LSS", "LEQ", "GTR", "GEQ", "LPARENT",
+						"RPARENT", "LBRACK", "RBRACK", "PERIOD", "COMMA", "SEMICOLON", "ASSIGN", "PERCENT"};
+
+#endif
